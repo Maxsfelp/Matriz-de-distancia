00b9ccd84674e970aa66f1528e98e4e811e95938
Maxsfelp <felipemaxsuel74@gmail.com>
2020-10-15 22:24:33 -0300

    Corrigindo erro no savefig

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ G = nx.Graph()
 G = nx.read_gml("football/football.gml")
 
 nx.draw(G, with_labels=True, font_weight='bold', width=1)
+plt.savefig("grafo.png", dpi=100)
 plt.show()
-plt.savefig("grafo.png")
