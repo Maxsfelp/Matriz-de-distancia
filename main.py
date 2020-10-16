bff2e36fec1a25bba2b9625597d1566fdfebfafc
Maxsfelp <felipemaxsuel74@gmail.com>
2020-10-15 22:06:53 -0300

    Updade main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 G = nx.Graph()
 
-G = nx.read_gml("dolphins.gml")
+G = nx.read_gml("football/football.gml")
 
 nx.draw(G, with_labels=True, font_weight='bold', width=1)
 plt.show()
