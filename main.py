ce4cfac45c206303b2a3860b9d709bf41338ec34
Maxsfelp <felipemaxsuel74@gmail.com>
2020-10-19 22:49:08 -0300

    gerando matriz de distancia

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ G = nx.Graph()
 
 G = nx.read_gml("football/football.gml")
 
+matrix_dist = nx.floyd_warshall_numpy(G)
+
+print(matrix_dist)
+
 nx.draw(G, with_labels=True, font_weight='bold', width=1)
 plt.savefig("grafo.png", dpi=100)
 plt.show()
