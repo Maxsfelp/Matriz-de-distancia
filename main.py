c1c53b2669f3a1061a40a66786d97e116ba89e78
Maxsfelp <felipemaxsuel74@gmail.com>
2020-10-22 23:23:57 -0300

    buscando dados para analise

--- main.py
+++ main.py
@@ -5,10 +5,32 @@ import pandas as pd
 
 G = nx.Graph()
 
-G = nx.read_gml("football/football.gml")
+G = nx.read_gml("lesmis.gml")
 
 matrix_dist = nx.floyd_warshall_numpy(G)
 
+minimo = []
+maximo = []
+somatorio = []
+menor = 99999
+maior = 0
+soma = 0
+
+for i in range(0, (G.number_of_nodes()-1)):
+	for j in range(0, (G.number_of_nodes()-1)):
+		if(matrix_dist[i][j] < menor):
+			menor = matrix_dist[i][j]
+		if(matrix_dist[i][j] > maior):
+			maior = matrix_dist[i][j]
+		soma = soma + matrix_dist[i][j]
+	somatorio.append(soma)
+	minimo.append(menor)
+	maximo.append(maior)
+	soma = 0
+	menor = 999999
+	maior = 0
+
+print(somatorio)
 pd.DataFrame(matrix_dist).to_csv("matrix.csv", header=None, index=None)
 
 nx.draw(G, with_labels=True, font_weight='bold', width=1)
